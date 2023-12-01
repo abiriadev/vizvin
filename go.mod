a3ca6e230e34a75ba09e5210b8fbda893b073907
Abiria <abiria.dev@gmail.com>
2023-12-01 14:45:29 +0900

    feat: use bigram-plot

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/abiriadev/vizvin
 
 go 1.21.4
+
+require (
+	github.com/abiriadev/iris v0.0.0-20231130022123-680c21ccc4be // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mazznoer/colorgrad v0.9.1 // indirect
+	github.com/mazznoer/csscolorparser v0.1.2 // indirect
+)
--- main.go
+++ main.go
@@ -8,13 +8,37 @@ import (
 	"os"
 )
 
-func main() {
+func genplot(r bufio.Reader) [256][256]int {
+	var plot [256][256]int
+
+	last, e := r.ReadByte()
+
+	if e == io.EOF {
+		return plot
+	}
+
+	for {
+		curr, e := r.ReadByte()
+
+		if e == io.EOF {
+			break
+		}
+
+		fmt.Printf("%02x, %02x\n", last, curr)
+
+		last = curr
+	}
+
+	return plot
+}
+
+func openFile() bufio.Reader {
 	flag.Parse()
 
 	fn := flag.Arg(0)
 
 	if fn == "" {
-		return
+		panic("pass binary file to visualize")
 	}
 
 	f, err := os.Open(fn)
@@ -23,19 +47,11 @@ func main() {
 		panic(err)
 	}
 
-	r := bufio.NewReader(f)
+	return *bufio.NewReader(f)
+}
 
-	for i := 0; ; i++ {
-		b, e := r.ReadByte()
+func main() {
+	bin := openFile()
 
-		fmt.Printf("%02X ", b)
-
-		if i%20 == 19 {
-			fmt.Printf("\n")
-		}
-
-		if e == io.EOF {
-			break
-		}
-	}
+	_ = genplot(bin)
 }
